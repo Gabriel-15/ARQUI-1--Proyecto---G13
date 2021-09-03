d154aeaea5a4e98f4040dbc767c25e99916859aa
Walter Manolo Martinez Mateo <waltermateo23@outlook.com>
2021-09-03 03:55:22 -0600

    casi listo
    solo un par de cosas faltan

--- proyecto1/proyecto1.ino
+++ proyecto1/proyecto1.ino
@@ -1,62 +1,91 @@
-  const int b1=3;
-const int b2=4;
-const int derecha =5; int pwm1;
-const int izquierda =6; int pwm2;
-
-int boton1;
-int boton2;
-int potenciometro;
+int lento = 100;
+int rapido = 255; 
+int valor;
 void setup() {
-  // put your setup code here, to run once:
-pinMode(derecha,OUTPUT);
-pinMode(izquierda,OUTPUT);
-pinMode(boton1,INPUT);
-pinMode(boton2,INPUT);
-pinMode(13,OUTPUT);
+pinMode(11,OUTPUT);
+pinMode(10,OUTPUT);
+pinMode(9,OUTPUT);
+pinMode(6,OUTPUT);
+pinMode(5,OUTPUT);
+pinMode(3,OUTPUT);
+//leds
+pinMode(7,OUTPUT);
+pinMode(4,OUTPUT);
+pinMode(2,OUTPUT);
 
-digitalWrite(derecha,LOW);
-digitalWrite(izquierda,LOW);
 }
-
 void loop() {
- 
- int valPotenciometro;
- 
- boton1=digitalRead(b1);
- boton2=digitalRead(b2);
-
- if(boton1==HIGH){
-  valPotenciometro=analogRead(potenciometro);
-  pwm1=map(valPotenciometro,0,1023,0,255);
-  digitalWrite(13,LOW);
-  analogWrite(derecha,pwm1);
-  Serial.println(valPotenciometro);
-  Serial.println("derecha");
-   
-  }
-  else{
-    valPotenciometro=analogRead(potenciometro);
-    pwm1 = map(valPotenciometro,0,0,0,0);
-digitalWrite(13,HIGH);
-    analogWrite(derecha,pwm1);
-        
-    }
-  if(boton2==HIGH){
-    valPotenciometro=analogRead(potenciometro);
-    pwm2=map(valPotenciometro,0,1023,0,255);
-    digitalWrite(13,LOW);
-    analogWrite(izquierda,pwm2);
-    Serial.println(valPotenciometro);
-    Serial.println("izquierda");
-    
-    }  
-    else{
-      valPotenciometro=analogRead(potenciometro);
-      pwm2=map(valPotenciometro,0,0,0,0);
-      digitalWrite(13,HIGH);
-      analogWrite(izquierda,pwm2);
-      
-      }
-  
-
+adelante(rapido);
+delay(2000);
+atras(lento);
+delay(2000);
+parar();
+delay(1000);
+der(lento);
+delay(1000);
+izq(rapido);
+delay(1000);
 }
+int adelante(int n){
+  
+analogWrite(11, n);
+digitalWrite(10, LOW);
+analogWrite(9, n);
+digitalWrite(6, LOW);
+analogWrite(5, n);
+digitalWrite(3, LOW);
+//leds
+digitalWrite(7, LOW);
+digitalWrite(4, LOW);
+digitalWrite(2, LOW);
+}
+int atras(int n){
+  digitalWrite(11, LOW);
+analogWrite(10, n);
+digitalWrite(9, LOW);
+analogWrite(6, n);
+digitalWrite(5, LOW);
+analogWrite(3, n);
+//leds
+digitalWrite(7, LOW);
+digitalWrite(4, LOW);
+digitalWrite(2, LOW);
+  }
+  void parar(){
+digitalWrite(11, LOW);
+digitalWrite(10, LOW);
+digitalWrite(9, LOW);
+digitalWrite(6, LOW);
+digitalWrite(5, LOW);
+digitalWrite(3, LOW);
+//leds
+digitalWrite(7, HIGH);
+digitalWrite(4, LOW);
+digitalWrite(2, LOW);
+  }
+  int der(int n){
+analogWrite(11, n);
+digitalWrite(10, LOW);
+digitalWrite(9, LOW);
+digitalWrite(6, LOW);
+analogWrite(5, n);
+digitalWrite(3, LOW);
+//leds
+digitalWrite(7, LOW);
+digitalWrite(4, HIGH);
+digitalWrite(2, LOW);
+    
+    }
+    int izq(int n){
+digitalWrite(11, LOW);
+digitalWrite(10, LOW);
+analogWrite(9, n);
+digitalWrite(6, LOW);
+analogWrite(5, n);
+digitalWrite(3, LOW);
+//leds
+digitalWrite(7, LOW);
+digitalWrite(4, LOW);
+digitalWrite(2, HIGH);
+    
+    }
